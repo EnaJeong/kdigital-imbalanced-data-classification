27b99cad8fc9528e6d1ddaa32d1af1b75b33264a
jeejeongwon <54830788+jeejeongwon@users.noreply.github.com>
2021-03-05 10:06:10 +0900

    Add files via upload
    수정본1

--- Classification_KNN.py
+++ Classification_KNN.py
@@ -21,16 +21,16 @@ pd.set_option("display.max_columns", None)
 
 # In[9]:
 
-with open(f"./datasets/X_samples.pickle", "rb") as f:
+with open(f"./samples/X_samples.pickle", "rb") as f:
     X_samples = pickle.load(f)
     
-with open(f"./datasets/y_samples.pickle", "rb") as f:
+with open(f"./samples/y_samples.pickle", "rb") as f:
     y_samples = pickle.load(f)
     
-with open(f"./datasets/X_test.pickle", "rb") as f:
+with open(f"./samples/X_test.pickle", "rb") as f:
     X_test = pickle.load(f)
     
-with open(f"./datasets/y_test.pickle", "rb") as f:
+with open(f"./samples/y_test.pickle", "rb") as f:
     y_test = pickle.load(f)
 
 
@@ -50,8 +50,8 @@ cols = ['Accuracy', 'Precision', 'Recall', 'F1-Score', 'ROC_AUC']
 
 # In[ ]:
 
-# print(X_samples.get(data_list[0]))
-# print(y_samples.get(data_list[0]))
+print(X_samples.get(data_list[0]))
+print(y_samples.get(data_list[0]))
 
 
 # # KNN
@@ -72,7 +72,7 @@ data_dict = {}
 for i in data_list:
     print(i)
 #     clf.fit(X, y)
-    clf.fit(X_samples.get(data_list[data_list.index(i)]), y_samples.get(data_list[data_list.index(i)]))
+    clf.fit(X_samples.get(i), y_samples.get(i))
     y_pred = clf.predict(X_test)
     y_proba = clf.predict_proba(X_test)
     
@@ -87,122 +87,119 @@ from sklearn.metrics import confusion_matrix
 
 def evaluate(test, pred):
 
-    confusion_mtx = confusion_matrix(test, pred)
-    accuracy = accuracy_score(test, pred)
-    precision = precision_score(test, pred)
-    recall = recall_score(test, pred)
-    f1 = f1_score(test, pred)
+    confusion_mtx = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_pred, y_test)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
     roc_auc = roc_auc_score(y_test, y_proba[:, 1])
 
-    
     print('오차 행렬',confusion_mtx)
     print('\n정확도: {:.4f}, 정밀도: {:.4f}, 재현율: {:.4f}, fl 스코어: {:.4f}, roc_auc : {:.4f}'.format(accuracy, precision, recall, f1, roc_auc))
     
-pred = clf.predict(X_test)
 print(evaluate(y_test,pred))
 
 
 # # 현주님 코드
-
-# In[ ]:
-
-print('현주님 코드')
-for key, sample in X_samples.items(): #y_samples 해도 상관x
-    y = y_samples[key]
-    total = len(y)
-    counts = y.value_counts()
-    
-    print('=' * 100)
-    print(key)
-    print('=' * 100)
-    print(sample.head())
-    print('-' * 100)
-    print(counts)
-    print('-' * 30)
-    print(f"Total : {total}")
-    for idx in counts.index:
-        print(f"{idx} 비율 : {counts[idx] / total * 100:6.2f} %")
-    print('=' * 100)
-
-
-# In[ ]:
-
-
-def show_data_info(y):
-    total = len(y)
-    counts = y.value_counts()
-    print('=' * 80)
-    print(counts)
-    print('-' * 30)
-    for idx in counts.index:
-        print(f"{idx} 비율 : {counts[idx] / total * 100:6.2f} %")
-    print('=' * 80)
-
-def evaluate_model(clf, x_test, y_test):
-    y_proba = clf.predict_proba(x_test)
-    y_pred = clf.predict(x_test)
-    
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    fl = f1_score(y_test, y_pred)
-    roc_auc = roc_auc_score(y_test, y_proba[:, 1])
-    
-    print('=' * 80)
-    print('Confusion Matrix')
-    print(confusion_matrix(y_test, y_pred))
-    print('-' * 60)
-    print(f'Accuracy  : {accuracy}')
-    print(f'Precision : {precision}')
-    print(f'Recall    : {recall}')
-    print(f'F1-Score  : {fl}')
-    print('-' * 60)
-    print(classification_report(y_test,y_pred))
-    print('-' * 60)
-    print(f'ROC AUC : {roc_auc}')
-    print('=' * 80)
-    
-    return accuracy, precision, recall, fl, roc_auc
-
-
-# In[ ]:
-
-
-class_weights = ['Balanced', 'SqrtBalanced']
-
-for class_weight in class_weights:
-    X, y = X_samples['Raw'], y_samples['Raw']
-    
-    print('=' * 80)
-    print('Raw')
-    show_data_info(y)
-    
+#
+# # In[ ]:
+#
+# print('현주님 코드')
+# for key, sample in X_samples.items(): #y_samples 해도 상관x
+#     y = y_samples[key]
+#     total = len(y)
+#     counts = y.value_counts()
+#
+#     print('=' * 100)
+#     print(key)
+#     print('=' * 100)
+#     print(sample.head())
+#     print('-' * 100)
+#     print(counts)
+#     print('-' * 30)
+#     print(f"Total : {total}")
+#     for idx in counts.index:
+#         print(f"{idx} 비율 : {counts[idx] / total * 100:6.2f} %")
+#     print('=' * 100)
+#
+#
+# # In[ ]:
+#
+#
+# def show_data_info(y):
+#     total = len(y)
+#     counts = y.value_counts()
+#     print('=' * 80)
+#     print(counts)
+#     print('-' * 30)
+#     for idx in counts.index:
+#         print(f"{idx} 비율 : {counts[idx] / total * 100:6.2f} %")
+#     print('=' * 80)
+#
+# def evaluate_model(clf, x_test, y_test):
+#     y_proba = clf.predict_proba(x_test)
+#     y_pred = clf.predict(x_test)
+#
+#     accuracy = accuracy_score(y_test, y_pred)
+#     precision = precision_score(y_test, y_pred)
+#     recall = recall_score(y_test, y_pred)
+#     fl = f1_score(y_test, y_pred)
+#     roc_auc = roc_auc_score(y_test, y_proba[:, 1])
+#
+#     print('=' * 80)
+#     print('Confusion Matrix')
+#     print(confusion_matrix(y_test, y_pred))
+#     print('-' * 60)
+#     print(f'Accuracy  : {accuracy}')
+#     print(f'Precision : {precision}')
+#     print(f'Recall    : {recall}')
+#     print(f'F1-Score  : {fl}')
+#     print('-' * 60)
+#     print(classification_report(y_test,y_pred))
+#     print('-' * 60)
+#     print(f'ROC AUC : {roc_auc}')
+#     print('=' * 80)
+#
+#     return accuracy, precision, recall, fl, roc_auc
+#
+#
+# # In[ ]:
+#
+#
+# class_weights = ['Balanced', 'SqrtBalanced']
+#
+# for class_weight in class_weights:
+#     X, y = X_samples['Raw'], y_samples['Raw']
+#
+#     print('=' * 80)
+#     print('Raw')
+#     show_data_info(y)
+#
 #     clf = KNeighborsClassifier(n_neighbors = 5,n_jobs=-1)
-    clf = KNeighborsClassifier(n_neighbors = 5)
-    clf.fit(X, y)
-    print("classifier created")
-
-    print(f'KNN {key}')
-    result = evaluate_model(clf, X_test, y_test)
-    
-    clf[class_weight] = clf
-    results[class_weight] = result
-
-
-# In[ ]:
-
-
-cols = ['Accuracy', 'Precision', 'Recall', 'F1-Score', 'ROC_AUC']
-    
-evaluation = pd.DataFrame(results, index=cols)
-evaluation.index.set_names('Score', inplace=True)
-print(evaluation)
-
-
-# In[ ]:
-
-
-evaluation.plot(y=X_samples.keys(), kind="bar", figsize=(10, 5))
-plt.legend(loc=(1.01, 0.))
-plt.show()
-
+#     clf.fit(X, y)
+#     print("classifier created")
+#
+#     print(f'KNN {key}')
+#     result = evaluate_model(clf, X_test, y_test)
+#
+#     clf[class_weight] = clf
+#     results[class_weight] = result
+#
+#
+# # In[ ]:
+#
+#
+# cols = ['Accuracy', 'Precision', 'Recall', 'F1-Score', 'ROC_AUC']
+#
+# evaluation = pd.DataFrame(results, index=cols)
+# evaluation.index.set_names('Score', inplace=True)
+# print(evaluation)
+#
+#
+# # In[ ]:
+#
+#
+# evaluation.plot(y=X_samples.keys(), kind="bar", figsize=(10, 5))
+# plt.legend(loc=(1.01, 0.))
+# plt.show()
+#
